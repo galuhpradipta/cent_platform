08f0748e2b5bdbb26fe366658c0fd8769b6e00cc
Galuh Pradipta <galuh@koinworks.com>
2019-08-02 12:54:48 +0700

    add decimal specification

--- database/migrations/2019_08_02_055419_create_banks.php
+++ database/migrations/2019_08_02_055419_create_banks.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBanksTable extends Migration
+class CreateBanks extends Migration
 {
     /**
      * Run the migrations.
@@ -18,8 +18,8 @@ class CreateBanksTable extends Migration
             $table->unsignedInteger('company_id');
             $table->string('name');
             $table->string('code');
-            $table->decimal('initial_balance');
-            $table->decimal('balance');            
+            $table->decimal('initial_balance', 16, 2);
+            $table->decimal('balance', 16, 2);            
             $table->timestamps();
         });
     }
