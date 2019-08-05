886173ccefc037451f530853a3289cd026d05d99
Galuh Pradipta <galuh@koinworks.com>
2019-08-05 11:50:51 +0700

    adding bank_id field from migration

---
+++ database/migrations/2019_08_05_044955_add_column_bank_id_on_sales_orders.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnBankIdOnSalesOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sales_orders', function (Blueprint $table) {
+            $table->unsignedInteger('bank_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
